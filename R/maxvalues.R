15749092fc81d34168935d2675394f211058ad76
Hannah Bobst <hannahbobst@Hannahs-MacBook-Pro.local>
2023-11-09 15:36:21 -0800

    Added licence, updated roxygen tags, added imports

--- R/maxvalues.R
+++ R/maxvalues.R
@@ -7,9 +7,11 @@
 #' @param ... Additional function arguments specifying the groups. These arguments must be character values corresponding to column names.
 #' @param na.rm A Boolean variable matching the argument name "na.rm" in the "max" function. Default input is TRUE, which removes NA values.
 #' @return A tibble summarizing the maximum values of variable by groups.
-#'
-#' @import tidyverse
+#' @import magrittr
+#' @import dplyr
+#' @import tibble
 #' @examples
+#' library(tibble)
 #' ex_data <- tibble(main_groups = c(rep("Group1",5), rep("Group2",5), rep("Group3",5)), sub_groups = c("subgroup1", "subgroup2", "subgroup3", "subgroup3", "subgroup2","subgroup1", "subgroup1", "subgroup1", "subgroup1", "subgroup3", "subgroup2", "subgroup3", "subgroup2", "subgroup1", "subgroup1"), values1 = c(59.55907, 38.73754, 54.49307, 52.52262, 59.53699, NA, 42.09835, 35.06464, 57.96973, 57.52726, 57.64511, 38.66776, NA, 41.15472, NA), values2 = c(2,5,7,NA,35,8,NA,15,67,8,9,36,14,23,6))
 #' max_values(data = ex_data, main_groups, sub_groups, vars = values1:values2, na.rm = TRUE)
 #' max_values(data = ex_data, main_groups, vars = values1:values2, na.rm = FALSE)
